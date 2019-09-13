a7abbe688dea471000356d01c2f291c2299f10af
arvestad <arve@math.su.se>
2019-09-13 14:38:03 +0200

    Better handling of errors on the command-line parameters

--- dnctree/main.py
+++ dnctree/main.py
@@ -36,9 +36,31 @@ def cmd_line_args():
     return args
 
 
+def check_args(args):
+    '''
+    Impose constraints on arguments. Some badly chosen parameters result in program exit, 
+    others simply a correction to a valid parameter value.
+    '''
+
+    if args.max_clade_size <= 0.01:
+        sys.exit('Error: --max-clade-size cannot be that small')
+    elif args.max_clade_size > 1.0:
+        print('Warning: --max-clade-size cannot be that large, changing to 1.0', file=sys.stderr)
+        args.max_clade_size = 1.0
+
+    if args.base_case_size < 3:
+        sys.exit('Error: --base-case-size must be at least 3.')
+
+    if args.max_n_attempts < 1:
+        print('Warning: --max-n-attempts cannot be smaller than 1. Setting that parameter to 1 and continuing.', file=sys.stderr)
+        args.max_n_attempts = 1
+
+
 def main():
     try:
         args = cmd_line_args()
+        check_args(args)
+
         if args.format == 'guess':
             inputformat = guess_format(args.infile)
         else:
@@ -63,6 +85,8 @@ def main():
     try:
         t = divide_n_conquer_tree(msa, args.max_n_attempts, args.max_clade_size, args.base_case_size, args.first_triple, verbosity)
         print(t)
+    except AssertionError:
+        sys.exit(f'A bug has occured. Please report an issue on http://github.com/arvestad/dnctree, and include sample data.')
     except KeyboardInterrupt:
         sys.exit()
     except Exception as e:
