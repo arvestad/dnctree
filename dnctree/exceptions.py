83dc1d01bd68affba4e7b4ef7fdc71a774ad8cc1
arvestad <arve@csc.kth.se>
2020-04-09 11:38:46 +0200

    Application-specific exceptions. First two are for when two sequences dont share homologous positions.

---
+++ dnctree/exceptions.py
@@ -0,0 +1,18 @@
+'''
+Some exception classes specific for dnctree.
+'''
+
+class NoSharedCharactersError(Exception):
+    '''
+    Used to signal that two sequences actually have no homologous
+    positions. There are plenty of examples of this in Pfam alignments.
+    '''
+    pass
+
+
+class AllCharactersDifferentError(Exception):
+    '''
+    Signals that two aligned sequences have replacements/substitutions
+    in all positions. There are plenty of such examples in Pfam.
+    '''
+    pass
