161147aeac42d0e5f47276af230b01d69ff2e6bf
arvestad <arve@math.su.se>
2020-05-13 10:31:20 +0200

    Moved to separate repo

--- dnctree/stupid_tree.py
+++ dnctree/stupid_tree.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 import argparse
 
+lba_length = 1.0
+
 def even_steven(depth, branchlength):
     t, _ = even_steven_helper(depth, branchlength, idx=0, n_kids=3)
     return t + ';'
@@ -40,11 +42,34 @@ def create_node(idx, branchlength):
     return f'L{idx}:{branchlength}'
 
 
+def lba_model_tree(depth, long_branchlength, regular_branchlength):
+    '''
+    Create a tree that could be susceptible to long branch attraction (LBA).
+    '''
+    idx = 0
+    t1, idx = lba_model_helper(depth-1, long_branchlength, regular_branchlength, idx)
+    t2, idx = even_steven_helper(depth-1, regular_branchlength, idx, 2)
+    t3, idx = lba_model_helper(depth-1, long_branchlength, regular_branchlength, idx)
+    t4, idx = even_steven_helper(depth-1, regular_branchlength, idx, 2)
+
+    L = long_branchlength
+    t = f'(({t1},{t2}):{regular_branchlength},{t3}, {t4});'
+    return t
+
+def lba_model_helper(depth, long_branchlength, regular_branchlength, idx):
+    assert depth > 1
+    t1, idx = even_steven_helper(depth-1, regular_branchlength, idx, 2)
+    t2, idx = even_steven_helper(depth-1, regular_branchlength, idx, 2)
+    return f'({t1},{t2}):{long_branchlength}', idx
+
+
 def main():
     ap = argparse.ArgumentParser(description='Create non-random test trees for dnctree')
     ap.add_argument('-b', '--branchlength', type=float, metavar='b', default=0.1, help='Branchlength to use on all edges.')
     ap.add_argument('-d', '--depth', type=int, help='Bifurcating tree. Specify #levels in the tree')
     ap.add_argument('-c', '--caterpillar', type=int, metavar='n', help='Caterpillar-shaped tree. Specify #leaves.')
+    ap.add_argument('-l', '--lba-tree', type=int, metavar='n', help='Tree inducing(?) long branch attraction. Specify depth of the four subtrees.')
+    ap.add_argument('-L', type=float, metavar='b', default=lba_length, help=f'Length of a long branch in the LBA test case. Default is {lba_length}.')
     args = ap.parse_args()
 
     if args.depth:
@@ -53,8 +78,10 @@ def main():
     elif args.caterpillar:
         assert args.caterpillar > 2
         t = caterpillar(args.caterpillar, branchlength=args.branchlength)
+    elif args.lba_tree:
+        t = lba_model_tree(args.lba_tree, args.L, args.branchlength)
     else:
-        raise('No. You have to say what kind of tree to generate.')
+        ap.exit('No. You have to say what kind of tree to generate.')
     print(t)
 
 
