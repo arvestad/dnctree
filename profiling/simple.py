13a4c871ba858030706258dd9f6cbef000b7cf87
arvestad <arve@math.su.se>
2024-02-01 16:14:33 -0500

    Some changes in profiling code

--- profiling/simple.py
+++ profiling/simple.py
@@ -10,7 +10,7 @@ def dnc_command():
     seqs, _ = read_alignment('../testdata/s83_L500.phylip', 'guess', 'phylip', None, None)
 
     msa = dnctree.MSA(seqs)
-    t, _ = dnctree.divide_n_conquer_tree(msa, base_case_size=20)
+    t, _ = dnctree.divide_n_conquer_tree(msa, base_case_size=50)
     #    print(str(t))
 
 if __name__ == "__main__":
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     cProfile.run('dnc_command()', profiling_output)
 
     p = pstats.Stats(profiling_output)
-    p.strip_dirs().sort_stats(SortKey.TIME).print_stats(10)    
+    p.sort_stats(SortKey.TIME).print_stats(20)    
 
